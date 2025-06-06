632b80e193082da3d2aa1fbcf63f07cd37761f40
claudito <megabyt1507@gmail.com>
2025-06-06 15:55:41 -0500

    add ODBC driver configuration for SQL Server in database.php

--- config/database.php
+++ config/database.php
@@ -110,6 +110,7 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'odbc_driver' => env('DB_ODBC_DRIVER', 'ODBC Driver 18 for SQL Server')
         ],
 
         'sihce' => [
